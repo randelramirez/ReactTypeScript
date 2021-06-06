385ddcc97c9db6efa1103d1a9a23e024b667d820
Randel Ramirez <randelramirez1@gmail.com>
2021-06-06 15:36:34 +0800

    Add Initialize Cart Action

--- src/components/AppState.tsx
+++ src/components/AppState.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useReducer } from "react";
+import React, { createContext, useContext, useEffect, useReducer } from "react";
 
 interface CartItem {
   id: number;
@@ -24,10 +24,19 @@ export const AppStateContext = createContext(defaultStateValue);
 // we create a different context for set state (for performance reasons)
 // https://stackoverflow.com/questions/66776717/does-putting-state-and-dispatch-into-separate-context-providers-prevent-unnecess
 export const AppDispatchContext =
-  createContext<React.Dispatch<AddToCartAction> | undefined>(undefined);
+  createContext<
+    React.Dispatch<InitializeCartAction | AddToCartAction> | undefined
+  >(undefined);
 
 interface Action<T> {
   type: T;
+}
+
+interface InitializeCartAction extends Action<"INITIALIZE_CART"> {
+  //payload: AppStateValue; or we can access the interface property like below
+  payload: {
+    cart: AppStateValue["cart"];
+  };
 }
 
 interface AddToCartAction extends Action<"ADD_TO_CART"> {
@@ -36,31 +45,42 @@ interface AddToCartAction extends Action<"ADD_TO_CART"> {
   };
 }
 
-const reducer = (state: AppStateValue, action: AddToCartAction) => {
-  if (action.type === "ADD_TO_CART") {
-    const itemToAdd = action.payload.item;
-    const itemExist = state.cart.items.find((item) => item.id === itemToAdd.id);
+const reducer = (
+  state: AppStateValue,
+  action: InitializeCartAction | AddToCartAction
+) => {
+  switch (action.type) {
+    case "INITIALIZE_CART":
+      return { ...state, cart: action.payload.cart };
 
-    return {
-      ...state,
-      cart: {
-        ...state.cart,
-        items: itemExist
-          ? state.cart.items.map((item) => {
-              if (item.id === itemExist.id) {
-                return { ...item, quantity: itemExist.quantity + 1 };
-              } else {
-                return item;
-              }
-            })
-          : /*[...state.cart.items, itemToAdd]*/ [
-              ...state.cart.items,
-              { ...itemToAdd, quantity: 1 },
-            ],
-      },
-    };
+    case "ADD_TO_CART":
+      const itemToAdd = action.payload.item;
+      const itemExist = state.cart.items.find(
+        (item) => item.id === itemToAdd.id
+      );
+
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          items: itemExist
+            ? state.cart.items.map((item) => {
+                if (item.id === itemExist.id) {
+                  return { ...item, quantity: itemExist.quantity + 1 };
+                } else {
+                  return item;
+                }
+              })
+            : /*[...state.cart.items, itemToAdd]*/ [
+                ...state.cart.items,
+                { ...itemToAdd, quantity: 1 },
+              ],
+        },
+      };
+
+    default:
+      throw new Error("Uknown Action on the reducer");
   }
-  return state;
 };
 
 export const useStateDispatch = () => {
@@ -75,6 +95,21 @@ export const useStateDispatch = () => {
 
 const AppStateProvider: React.FC = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, defaultStateValue);
+
+  useEffect(() => {
+    const cart = window.localStorage.getItem("cart");
+    if (cart) {
+      dispatch({
+        type: "INITIALIZE_CART",
+        payload: { cart: JSON.parse(cart) },
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
+
   return (
     <AppStateContext.Provider value={state}>
       <AppDispatchContext.Provider value={dispatch}>
