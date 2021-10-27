9f6385df37deea233ab24f458c8308c373c599af
Randel Ramirez <randelramirez1@gmail.com>
2021-10-27 21:32:17 +0800

    Functional cart

--- BasicReactTS/src/components/Cart.tsx
+++ BasicReactTS/src/components/Cart.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {createRef} from "react";
 import { FiShoppingCart } from "react-icons/fi";
 import CartCSS from "./Cart.module.css";
 import { AppStateContext } from "../appStateContext";
@@ -10,11 +10,13 @@ interface State {
 }
 
 class Cart extends React.Component<Props, State> {
+  containerRef: React.RefObject<HTMLDivElement>;
   constructor(props: Props) {
     super(props);
     this.state = {
       isOpen: false,
     };
+    this.containerRef = createRef();
   }
 
   handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
@@ -24,6 +26,22 @@ class Cart extends React.Component<Props, State> {
     this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
   };
 
+  handler = (event: MouseEvent) => {
+    // Node is a base of HTMLElement, HTMLDivElement etc.
+    if(this.containerRef.current && !this.containerRef.current.contains(event.target as Node)){
+      // close the cart if you click outside of it
+      this.setState({isOpen: false})
+    }
+  }
+  
+  componentDidMount() {
+    document.addEventListener('mousedown',this.handler)
+  }
+  
+  componentWillUnmount() {
+    document.removeEventListener('mousedown',this.handler)
+  }
+
   render() {
     return (
       <AppStateContext.Consumer>
@@ -32,7 +50,7 @@ class Cart extends React.Component<Props, State> {
             return sum + item.quantity;
           }, 0);
           return (
-            <div className={CartCSS.cartContainer}>
+            <div className={CartCSS.cartContainer} ref={this.containerRef}>
               <button
                 className={CartCSS.button}
                 type="button"
---
+++ BasicReactTS/src/components/CartFunctional.tsx
@@ -0,0 +1,74 @@
+﻿import React, { useEffect, useRef, useState } from "react";
+import { FiShoppingCart } from "react-icons/fi";
+import CartCSS from "./Cart.module.css";
+import { AppStateContext } from "../appStateContext";
+
+interface Props {}
+
+export const Cart: React.FC<Props> = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    // if ((e.target as HTMLElement).nodeName === 'SPAN') {
+    //   (e.target as HTMLSpanElement).;
+    // }
+    // this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
+
+    setIsOpen((currentState) => !currentState);
+  };
+
+  const handler = (event: MouseEvent) => {
+    // Node is a base of HTMLElement, HTMLDivElement etc.
+    if (
+      containerRef.current &&
+      !containerRef.current.contains(event.target as Node)
+    ) {
+      // close the cart if you click outside of it
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handler);
+    return () => document.removeEventListener("mousedown", handler);
+  }, []);
+
+  return (
+    <AppStateContext.Consumer>
+      {(state) => {
+        const itemsCount = state.cart.items.reduce((sum, item) => {
+          return sum + item.quantity;
+        }, 0);
+        return (
+          <div className={CartCSS.cartContainer} ref={containerRef}>
+            <button
+              className={CartCSS.button}
+              type="button"
+              onClick={handleClick}
+            >
+              <FiShoppingCart />
+              <span>{itemsCount} pizza(s)</span>
+            </button>
+            <div
+              className={CartCSS.cartDropDown}
+              style={{
+                display: isOpen ? "block" : "none",
+              }}
+            >
+              <ul>
+                {state.cart.items.map((item) => (
+                  <li key={item.id}>
+                    {item.name} &times; {item.quantity}{" "}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        );
+      }}
+    </AppStateContext.Consumer>
+  );
+};
+
+export default Cart;
